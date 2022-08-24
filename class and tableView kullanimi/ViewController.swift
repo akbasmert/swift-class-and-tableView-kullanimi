8d3872c2116c2707a74e558a54bfa6ea2484a432
mert <mert.ma346@gmail.com>
2022-08-24 20:20:52 +0300

    tableView için gerekli olan kodlar eklendi.

--- class and tableView kullanimi/ViewController.swift
+++ class and tableView kullanimi/ViewController.swift
@@ -7,14 +7,33 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     @IBOutlet weak var tableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        // TABLEVİEW İÇİN GEREKLİ
+        tableView.delegate = self
+        tableView.dataSource = self
     }
+    
+    
+    // TABLEVİEW İÇİN GEREKLİ OLAN İKİ FONKSİYONU EKLEDİK
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = "Text"
+        return cell
+        
+    }
+    
+    
 
 
 }
