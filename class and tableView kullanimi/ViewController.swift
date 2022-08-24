5b60da643b7b81772d7926a8e1d7f643e215d5f7
mert <mert.ma346@gmail.com>
2022-08-24 19:46:32 +0300

    Initial Commit

---
+++ class and tableView kullanimi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  class and tableView kullanimi
+//
+//  Created by Mert AKBAŞ on 24.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
